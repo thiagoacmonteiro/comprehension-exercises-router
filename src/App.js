1a9989a02a2a8f967f9d9b159cd361241267559d
Thiago Monteiro <thiagoacmonteiro@outlook.com>
2021-10-19 16:56:34 -0300

    Exercise 6 solved

--- src/App.js
+++ src/App.js
@@ -2,19 +2,23 @@ import React, { Component } from 'react';
 import Home from './components/Home';
 import About from './components/About';
 import Users from './components/Users';
-// Exercise 1: Make the aplication navegable using BrowserRouter
+import { Link } from 'react-router-dom';
+// Exercise 1: Make the aplication navegable using 'BrowserRouter'
 import { BrowserRouter, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        {/* Exercise 2: Create a route to Home */}
+        {/* Exercise 2: Create a route to 'Home' */}
         <Route path exact="/" component={ Home } /> {/* Exercise 4: Change the way of routing for 'Home' to avoid rendering another component */}
-        {/* Exercise 3: Create a route to About */}
+        {/* Exercise 3: Create a route to 'About' */}
         <Route path="/about" component={ About } />
-        {/* Exercise 5: Create a route to About */}
+        {/* Exercise 5: Create a route to 'Users' */}
         <Route path="/users" component={ Users } />
+        {/* Exercise 6: Create links to 'About' and 'Users' */}
+        <Link to="/about">About </Link>
+        <Link to="/users">Users</Link>
       </BrowserRouter>
     );
   }
