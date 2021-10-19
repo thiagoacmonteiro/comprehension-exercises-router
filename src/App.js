786714f71d0a65b2b0840d8f4279dc325b9a5ed5
Thiago Monteiro <thiagoacmonteiro@outlook.com>
2021-10-19 16:39:10 -0300

    Exercise 2 solved

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import Home from './components/Home';
 // Exercise 1: Make the aplication navegable using BrowserRouter
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Home />
+        {/* Exercise 2: Create a route to Home */}
+        <Route path="/" component={ Home } />
       </BrowserRouter>
     );
   }
