880deb20783db8a50cc1126fe5e613803edb4ba6
Thiago Monteiro <thiagoacmonteiro@outlook.com>
2021-10-19 17:30:45 -0300

    Exercise 7 solved

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
+// Exercise 7: Change this component to receive props from App
 class Users extends Component {
   render() {
+    const { greetingsMessage } = this.props
+
     return (
       <div>
         <h2>Users</h2>
-        <p> My awesome Users component </p>
+        <p> { greetingsMessage }, My awesome Users component </p>
         <Link to="/">Home </Link>
       </div>
     );
