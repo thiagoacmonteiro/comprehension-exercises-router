5048e87326cea510a12538a5fa0ee1375e27ca42
Thiago Monteiro <thiagoacmonteiro@outlook.com>
2021-10-19 21:01:46 -0300

    Exercise 10 solved

--- src/App.js
+++ src/App.js
@@ -5,20 +5,24 @@ import Users from './components/Users';
 import { Link } from 'react-router-dom';
 // Exercise 1: Make the aplication navegable using 'BrowserRouter'
 import { BrowserRouter, Route } from 'react-router-dom';
+import { Switch } from 'react-router';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        {/* Exercise 2: Create a route to 'Home' */}
-        <Route path exact="/" component={ Home } /> {/* Exercise 4: Change the way of routing for 'Home' to avoid rendering another component */}
-        {/* Exercise 3: Create a route to 'About' */}
-        <Route path="/about" component={ About } />
-        {/* Exercise 5: Create a route to 'Users' */}
-        <Route path="/users/:id" render={(props) => <Users { ...props } greetingsMessage="Good Morning!" /> } /> {/* Exercise 8: Assign a string to the param 'greetingsMessage' to be seen by 'Users' */}
-        {/* Exercise 6: Create links to 'About' and 'Users' */}
+        {/* Exercise 10: Encapsulate routes inside the Switch and order from the most specific to the most generic */}
+        <Switch>
+          {/* Exercise 3: Create a route to 'About' */}
+          <Route path="/about" component={ About } />
+          {/* Exercise 5: Create a route to 'Users' */}
+          <Route path="/users/:id" render={(props) => <Users { ...props } greetingsMessage="Good Morning!" /> } /> {/* Exercise 8: Assign a string to the param 'greetingsMessage' to be seen by 'Users' */}
+          {/* Exercise 2: Create a route to 'Home' */}
+          <Route path exact="/" component={ Home } /> {/* Exercise 4: Change the way of routing for 'Home' to avoid rendering another component */}
+          {/* Exercise 6: Create links to 'About' and 'Users' */}
+        </Switch>
         <Link to="/about">About </Link>
-        <Link to="/users">Users</Link>
+        <Link to="/users">Users</Link>        
       </BrowserRouter>
     );
   }
